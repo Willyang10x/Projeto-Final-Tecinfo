0d8ad0c6b298ce7764a9afa5135c910403978f1f
gustavohenriqdev <gustavohen.dev@gmail.com>
2024-09-03 21:34:31 -0300

    Atualização da pagina de compra
    correção de espaçamentos, códigos e bugs.
    adição da barra de detalhes

--- pagina-de-compra/app.js
+++ pagina-de-compra/app.js
@@ -1,14 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('/Header/header.html')
+    fetch('../Header/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;
         });
 
-    fetch('/Footer/Footer.html')
+    fetch('../Footer/Footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer-placeholder').innerHTML = data;
       });
 
+});
+
+let elementoDetalhes = document.querySelectorAll('.sobre');
+
+elementoDetalhes.forEach(function (sobre) {
+    sobre.addEventListener('click', function() {
+        sobre.classList.toggle('active')
+    })
 });
