1b4496e7e2f4ef759960ae3ad768601d61626c30
Gustavo Henrique <gustavoprincipal60@gmail.com>
2024-09-06 13:33:51 -0300

    mudança de design da pagina de pagamento

--- Pagina-de-pagamento/app.js
+++ Pagina-de-pagamento/app.js
@@ -1,13 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('../Footer/Footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('footer-placeholder').innerHTML = data;
-      });
 
-});
-
-// Script para exibir/esconder os campos do cartão de crédito conforme a escolha da forma de pagamento
+// Exibir/esconder os campos do cartão de crédito conforme a escolha da forma de pagamento
 document.querySelectorAll('input[name="payment-method"]').forEach((input) => {
     input.addEventListener('change', function() {
         if (this.value === 'cartao') {
