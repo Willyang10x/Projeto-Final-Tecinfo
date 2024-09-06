4b50e67c11cdce61d377908bce0fa44538ee4b18
Gustavo Henrique <gustavoprincipal60@gmail.com>
2024-09-06 13:34:14 -0300

    Correção de bug da pagina sobre

--- pagina-sobre/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('../Footer/Footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('footer-placeholder').innerHTML = data;
-      });
-
-});
