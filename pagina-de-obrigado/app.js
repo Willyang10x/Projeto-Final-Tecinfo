425481c2b56accf734da636983bc222fd3f7aae2
gustavohenriqdev <gustavohen.dev@gmail.com>
2024-09-05 21:44:35 -0300

    atualização da pagina

--- pagina-de-obrigado/app.js
+++ pagina-de-obrigado/app.js
@@ -1,10 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('../Header/header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header-placeholder').innerHTML = data;
-        });
-
     fetch('../Footer/Footer.html')
     .then(response => response.text())
     .then(data => {
