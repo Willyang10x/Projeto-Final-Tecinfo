0b6fc8a439c140899f8ab4db57e37a014324000e
gustavohenriqdev <gustavohen.dev@gmail.com>
2024-09-13 19:42:48 -0300

    ..

--- Components/pagina-de-categoria/app.js
+++ Components/pagina-de-categoria/app.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('../Header/header.html')
+    fetch('/Components/Header/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;
         });
 
-    fetch('../Footer/Footer.html')
+    fetch('/Components/Footer/Footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer-placeholder').innerHTML = data;
