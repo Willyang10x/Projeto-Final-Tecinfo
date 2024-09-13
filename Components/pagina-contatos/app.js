a2a69401f59ad4b4af0423da60e8836ee78cf4c4
gustavohenriqdev <gustavohen.dev@gmail.com>
2024-09-13 20:10:29 -0300

    atualização

--- Components/pagina-contatos/app.js
+++ Components/pagina-contatos/app.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('../Footer/Footer.html')
+  fetch('../Header/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-placeholder').innerHTML = data;
+      });  
+  
+  fetch('../Footer/Footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer-placeholder').innerHTML = data;
